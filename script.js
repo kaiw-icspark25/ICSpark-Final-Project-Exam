618ac0b20cffca5271c95d6c0f67fbce0c3dd006
kaiw-icspark25 <chinow898@gmail.com>
2025-04-13 04:11:22 +0000

    KAI

---
+++ script.js
@@ -0,0 +1,23 @@
+let money=0,
+    cps=0,
+    clicker=1, //money per click
+    cursorBought=0,
+    cursorPrice=0,
+    grannyBought=0,
+    grannyPrice=0,
+    farmBought=0,
+    farmPrice=0,
+    bakeryBought=0,
+    bakeryPrice=0,
+    mineBought=0,
+    minePrice=0,
+    multiply=1.1;//amount multiplying
+    
+//adds money per click   
+var element= document.getElementById("makeCookie");
+element.addEventListener("click", function(){money+=clicker});
+//manufacturer makes prices and values
+function manufacturer(){
+    document.
+}
+
